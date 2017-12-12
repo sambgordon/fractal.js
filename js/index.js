37c70524c10d092dbb1e9e61825c633784e92829
DevBySam <samgordondev@gmail.com>
2017-12-12 15:05:05 -0700

    Fixed spacing

--- js/index.js
+++ js/index.js
@@ -11,44 +11,53 @@
 void function () {
 	//Declare variables travel, branchWidth, canvas, and the canvas context
 	var travel = 100 , branchWidth = 10
+	
 	//Draw canvas and set 2D context
 	var canvas = document.getElementById('mycanvas')
 	var ctx = canvas.getContext('2d')
+	
 	//set width and height of canvas to fill screen
 	canvas.width = window.innerWidth
 	canvas.height = window.innerHeight
   ctx.globalCompositeOperation;
 	var fractalTree = function () {
+		
 		/* accesses clearRect() method of Canvas API with starting point (0,0) and size set relative to
 		window width and height
 		*/
 		ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-		//Defines placement of tree relative to window, also
+		
+		//Defines placement of tree relative to window
 		drawTree(ctx, (window.innerWidth/2), (window.innerHeight/1), 40, -Math.PI/2, travel+10, branchWidth)
+		
 	}
-	//Define drawTree var as function that has following variables as parameters
+	//Define drawTree function
 	var drawTree = function (ctx, startX, startY, length, angle, travel, branchWidth) {
 		var rand = Math.random, dept,
 			newLength, newAngle,
 			mainBranch = 3,
 			endX, endY, maxAngle = Math.PI /2;
+		
 		//Creates new canvas path
 		ctx.beginPath()
-		//Sets new trees to defined x and y coordinates - placed in center
 		ctx.moveTo(startX, startY)
+		
 		//defines x and y end values by start values
 		endX = startX + length * Math.cos(angle)
 		endY = startY + length * Math.sin(angle)
+		
 		//uses lineCap canvas property to round the lines
 		ctx.lineCap = 'round'
 		ctx.lineWidth = branchWidth
 		ctx.lineTo(endX, endY)
+		
 		//sets white color for generated tree and branches
 		ctx.strokeStyle = 'white'
 		ctx.stroke()
 		newTravel = travel
 		if (!newTravel) return
 		subBranches = mainBranch -1
+		
 		/*This is where the recursion occurs
 		Each new child branch is 7/10 the size of its parent branch,
 		so they are generated infinitely until they "fizzle out". */
@@ -58,11 +67,13 @@ void function () {
 			that is based on the max angle multiplied by a random value.
 			The value can't be negative because the branches will turn in onto themselves!
 			*/
+			
 			newAngle = angle + rand()*maxAngle - maxAngle*.5
 			newLength = length * (.7 + rand()*.5)
 			/*uses setTimeout method with input as the drawTree function that draws tree
 			and its parameters. Expression is evaluated after 60 ms.
 			*/
+			
 			setTimeout(function () {
 				drawTree(ctx, endX, endY, newLength, newAngle, newTravel, branchWidth);
 				}, 60)
@@ -70,6 +81,7 @@ void function () {
 	}
 	/*adds event listener that detects whether DOM has loaded, runs fractalTree regardless if content has loaded
 	so it triggers more quickly */
+	
 	document.addEventListener('DOMContentLoaded',function(){
 		fractalTree()
 	}, false);
